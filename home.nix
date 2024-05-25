0eea01e752d6a8c3b565c3fefe62bc7ea111518b
izzudd <36723840+izzudd@users.noreply.github.com>
2024-05-25 20:06:40 +0700

    add zoxide and fzf, update to 23.11

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
   home.homeDirectory = "/home/izzudd";
 
   nixpkgs.config.allowUnfree = true;
+  targets.genericLinux.enable = true;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -15,7 +16,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -25,6 +26,11 @@
     hello
     neofetch
     htop
+
+    nodejs_20
+    netlify-cli
+    yarn
+    go
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -59,8 +65,12 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
-    enableZshIntegration = true;
   };
+  programs.zoxide = {
+    enable = true;
+    options = [ "--cmd cd" ];
+  };
+  programs.fzf.enable = true;
   programs.zsh = {
     enable = true;
     shellAliases = {
